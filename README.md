f3dbaae21b3ac4cbb1925a047fff1f48a76acec4
mobamoh <digiwilds@gmail.com>
2023-07-26 18:14:26 +0200

    created router and added modd.

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # snapsight
 SnapSight is a captivating photo gallery site meticulously crafted using the power of Go 🐹
+
+# tools
+
+[//]: # (todo migrate to https://github.com/cosmtrek/air)
+go install github.com/cortesi/modd/cmd/modd@latest 
+
+
+
+
--- main.go
+++ main.go
@@ -7,14 +7,41 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/", handleFun)
+	var router Router
 	fmt.Println("server listening at :1313...")
-	if err := http.ListenAndServe(":1313", nil); err != nil {
+	if err := http.ListenAndServe(":1313", router); err != nil {
 		log.Fatal(err)
 	}
 
 }
 
-func handleFun(w http.ResponseWriter, r *http.Request) {
+type Router struct {
+}
+
+func (Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/":
+		homeHandler(w, r)
+	case "/contact":
+		contactHandler(w, r)
+	case "/faq":
+		faqHandler(w, r)
+	default:
+		http.Error(w, "Page not found.", http.StatusNotFound)
+	}
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(w, "<h1>Hello Server</h1>")
+}
+
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1>Contact info</h1><p>contact me at mo@mail.com</p>")
+}
+
+func faqHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>FAQ</h1><p1>Some frequently asked questions and their answers</p1>")
 }
