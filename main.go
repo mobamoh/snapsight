f66cd6ab4cfa19e024512445747df6b3cb69b02b
mobamoh <digiwilds@gmail.com>
2023-07-28 18:13:29 +0200

    added templates and refactored code.

--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 	"github.com/go-chi/chi/v5"
+	"html/template"
 	"log"
 	"net/http"
+	"path/filepath"
 )
 
 func main() {
@@ -12,14 +14,6 @@ func main() {
 	router.Get("/", homeHandler)
 	router.Get("/contact", contactHandler)
 	router.Get("/faq", faqHandler)
-	router.NotFound(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "This page is maybe disappeared in a black hole!! 🕳️")
-	})
-
-	router.Get("/param/{id}", func(w http.ResponseWriter, r *http.Request) {
-		param := chi.URLParam(r, "id")
-		fmt.Fprintf(w, "Hello %s", param)
-	})
 
 	fmt.Println("server listening at :1313...")
 	if err := http.ListenAndServe(":1313", router); err != nil {
@@ -29,16 +23,27 @@ func main() {
 }
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<h1>Hello Server</h1>")
+	renderTemplate(w, "home.html")
 }
 
 func contactHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<h1>Contact info</h1><p>contact me at mo@mail.com</p>")
+	renderTemplate(w, "contact.html")
 }
 
 func faqHandler(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "faq.html")
+}
+
+func renderTemplate(w http.ResponseWriter, templateName string) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprint(w, "<h1>FAQ</h1><p1>Some frequently asked questions and their answers</p1>")
+	files, err := template.ParseFiles(filepath.Join("templates", templateName))
+	if err != nil {
+		log.Printf("error parsing template: %v", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+	}
+	err = files.Execute(w, nil)
+	if err != nil {
+		log.Printf("error executing template: %v", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+	}
 }
